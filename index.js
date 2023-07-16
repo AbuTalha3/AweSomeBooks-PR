92afb03afbd91e39b5afbf77d0e25449a6b0280e
AbuTalha3 <127955219+AbuTalha3@users.noreply.github.com>
2023-07-16 12:54:40 +0500

    Fix the linter issues

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable class-methods-use-this */
 /* eslint-disable no-use-before-define */
+/* eslint-disable max-classes-per-file */
 class Book {
   constructor(title, author) {
     this.title = title;
